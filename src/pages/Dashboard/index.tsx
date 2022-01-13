381483d1be4b26ea241d5d45f48491a7cee15e16
amirreza_bahoush <a.bahoush@omid.ir>
2022-01-13 23:44:35 +0330

    fix: change user dettails

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,46 +1,44 @@
 import React from "react";
-import { Typography, Table } from "antd";
+import { Typography, Table, Row, Col } from "antd";
 import { StyledCard } from "./dashboard.styled";
-import { NavLink, useNavigate } from "react-router-dom";
 
 const Dashboard: React.FC = () => {
-
 	const columns = [
 		{
-			title: 'ردیف',
-			dataIndex: 'row',
-			key: 'row',
+			title: "ردیف",
+			dataIndex: "row",
+			key: "row",
 		},
 		{
-			title: 'نوع کاربر',
-			dataIndex: 'userType',
-			key: 'userType',
+			title: "نوع کاربر",
+			dataIndex: "userType",
+			key: "userType",
 		},
 		{
-			title: 'کد ملی',
-			dataIndex: 'nationalId',
-			key: 'nationalId',
+			title: "کد ملی",
+			dataIndex: "nationalId",
+			key: "nationalId",
 		},
 		{
-			title: 'شماره همراه',
-			dataIndex: 'mobile',
-			key: 'mobile',
+			title: "شماره همراه",
+			dataIndex: "mobile",
+			key: "mobile",
 		},
 		{
-			title: 'ایمیل',
-			dataIndex: 'email',
-			key: 'email',
+			title: "ایمیل",
+			dataIndex: "email",
+			key: "email",
 		},
 	];
-	
+
 	const dataSource = [
 		{
-			row: '1',
-			userType: 'ایرانی',
-			nationalId: '0014202077',
-			mobile: '09121234567',
-			email: 's.k@gmail.com'
-		}
+			row: "1",
+			userType: "ایرانی",
+			nationalId: "0014202077",
+			mobile: "091212345127",
+			email: "s.k@gmail.com",
+		},
 	];
 
 	return (
@@ -52,12 +50,38 @@ const Dashboard: React.FC = () => {
 			}
 			bordered={false}
 		>
-			<Table
+			<Row>
+				<Col xs={12} className="bold mt-1">
+					ملیت:
+				</Col>
+				<Col xs={12} className="mt-1">
+					ایرانی
+				</Col>
+				<Col xs={12} className="bold my-1">
+					کد ملی:
+				</Col>
+				<Col xs={12} className="my-1">
+					0014202077
+				</Col>
+				<Col xs={12} className="bold my-1">
+					شماره همراه:
+				</Col>
+				<Col xs={12} className="my-1">
+					091212345127
+				</Col>
+				<Col xs={12} className="bold my-1">
+					ایمیل:
+				</Col>
+				<Col xs={12} className="my-1">
+					s.k@gmail.com
+				</Col>
+			</Row>
+			{/* <Table
 				dataSource={dataSource}
 				columns={columns}
 				pagination={false}
 				scroll={{ x: 240 }}
-			/>
+			/> */}
 		</StyledCard>
 	);
 };
